2944b219d003b84a2020ab68afd28a915e9ca992
r-strilets <55804526+r-strilets@users.noreply.github.com>
2022-10-07 18:05:27 +0300

    task 1-3

---
+++ src/components/App.jsx
@@ -0,0 +1,85 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  addContact = e => {
+    e.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+    console.log(newContact);
+    this.setState({
+      contacts: [...this.state.contacts, newContact],
+      number: '',
+      name: '',
+    });
+  };
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  contactsFilter = e => {
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+  render() {
+    const { contacts } = this.state;
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toString().toLowerCase())
+    );
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form action="" onSubmit={this.addContact}>
+          <input
+            onChange={this.onChangeInput}
+            type="text"
+            name="name"
+            value={this.state.name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <input
+            type="tel"
+            name="number"
+            onChange={this.onChangeInput}
+            value={this.state.number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button>Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <h3>Find contacts by name</h3>
+        <input type="text" onChange={this.contactsFilter} />
+        <ul>
+          {filteredContacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
