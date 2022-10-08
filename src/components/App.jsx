6a02c30ec4d0dce519ccf1a0b429c8818cee34ea
r-strilets <str.komar@gmail.com>
2022-10-09 02:35:47 +0300

    add form component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+
+import { ContactForm } from './ContactForm/ContactForm';
 export class App extends Component {
   state = {
     contacts: [
@@ -9,30 +10,15 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
-
-  addContact = e => {
-    e.preventDefault();
-
-    const newContact = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
-    };
-    console.log(newContact);
-    this.setState({
-      contacts: [...this.state.contacts, newContact],
-      number: '',
-      name: '',
+  getNewContactsArray = newContact => {
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, newContact],
+      };
     });
   };
-  onChangeInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
+
   contactsFilter = e => {
     this.setState({
       filter: e.target.value,
@@ -44,30 +30,11 @@ export class App extends Component {
         .toLowerCase()
         .includes(this.state.filter.toString().toLowerCase())
     );
+    console.log(this.state.contacts);
     return (
       <>
-        <h2>Phonebook</h2>
-        <form action="" onSubmit={this.addContact}>
-          <input
-            onChange={this.onChangeInput}
-            type="text"
-            name="name"
-            value={this.state.name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <input
-            type="tel"
-            name="number"
-            onChange={this.onChangeInput}
-            value={this.state.number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <button>Add contact</button>
-        </form>
+        <h1>Phonebook</h1>
+        <ContactForm data={this.getNewContactsArray} />
         <h2>Contacts</h2>
         <h3>Find contacts by name</h3>
         <input type="text" onChange={this.contactsFilter} />
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  addContact = e => {
+    e.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+    this.props.data(newContact);
+    this.setState({
+      number: '',
+      name: '',
+    });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <>
+        <form action="" onSubmit={this.addContact}>
+          <input
+            onChange={this.onChangeInput}
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <input
+            type="tel"
+            name="number"
+            onChange={this.onChangeInput}
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button>Add contact</button>
+        </form>
+      </>
+    );
+  }
+}
